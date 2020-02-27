c93ece6e66a04be2d4fd0da6f72bc3f02d07bfb6
Teresa Quintano <teresaquintano@gmail.com>
2020-02-27 10:06:56 +0100

    listado y mocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from "@angular/forms";
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from '@components/heroes/heroes.component';
 
@@ -12,7 +15,8 @@ import { HeroesComponent } from '@components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Hero} from "@share/interfaces/hero";
+import {HEROES} from "@mocks/mock-heroes";
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+    heroes = HEROES;
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
