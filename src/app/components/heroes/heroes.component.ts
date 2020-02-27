8a910b72efadc3fc93f82df7a7a39a13c24718fd
Teresa Quintano <teresaquintano@gmail.com>
2020-02-27 15:21:08 +0100

    componente detalle

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,12 +10,20 @@ import {HEROES} from "@mocks/mock-heroes";
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+    // El array de heroes
     heroes = HEROES;
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
 
+    // El heroe que creo y que luego se lo paso con ngModel
+    // hero: Hero = {
+    //     id: 1,
+    //     name: 'Windstorm'
+    // };
+    // El heroe que selecciono y que luego se lo paso con ngModel
+    selectedHero: Hero;
+    onSelect(hero: Hero): void {
+        // al seleccionarlo coge los datos de ese heroe
+        this.selectedHero = hero;
+    }
   constructor() { }
 
   ngOnInit(): void {
